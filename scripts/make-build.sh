d1fcbeea002880c6f967c54a81906570d6ed2221
playerg9 <g9yout@gmail.com>
2024-04-11 11:27:46 +0200

    added simple build script

---
+++ scripts/make-build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(realpath "$(dirname "$(realpath "$0")")/..")"
+
+# create build directory
+mkdir -p "build/"
+
+# remove old source-code
+[[ -f "build/img" ]] && rm "build/img"
+[[ -f "build/requirements.txt" ]] && rm "build/requirements.txt"
+[[ -d "build/src/" ]] && rm -rf "build/src/"
+
+PIPENV_VERBOSITY=-1 pipenv requirements > build/requirements.txt
+
+# copy source code
+cp -r src/img/ build/src/
+
+# install dependencies into (new) copied source-code directory
+python3 -m pip install -q -r build/requirements.txt -t build/src/ --compile --disable-pip-version-check
+rm -rf build/src/*.dist-info
+
+# cleanup of necessary packages
+rm -rf build/src/bin/  # executables/scripts
+rm -rf build/src/pygments/  # syntax highlighting
+rm -rf build/src/markdown_it/  # markdown parser
+rm -rf build/src/urllib3/  # builtin urllib is enough
+
+# make archive (from the new source-code directory)
+python3 -m zipapp --compress --python "/usr/bin/env -S python3 -X utf8 -X faulthandler -B -O" build/ --main src.__main__:main --output build/img
