6e2d5d61ee2d269e5d5c7a4f70c5bb1e5c09edf4
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-10-16 16:53:09 +0200

    fixed slight flaw in catch_sigint.py

--- src/img/core/catch_sigint.py
+++ src/img/core/catch_sigint.py
@@ -23,12 +23,19 @@ class CatchSigInt:
         self._restore_handler()
         self._event = None
 
+    def continue_catching(self) -> None:
+        if self._old_handler is not None:
+            raise RuntimeError()
+        self._override_handler()
+
     def _override_handler(self):
         self._old_handler = signal.getsignal(signal.SIGINT)
         signal.signal(signal.SIGINT, self._handler)
 
     def _handler(self, _signum, _frame):
-        self._event.set()
+        self._event.set()  # set for a graceful shutdown
+        self._restore_handler()  # default handler for forced shutdown
 
     def _restore_handler(self):
         signal.signal(signal.SIGINT, self._old_handler)
+        self._old_handler = None
